6fd86712ac198f0ee7a8f5008d0aefd5a67965fa
Mayara Pinheiro <mayarapinheirodesousa@hotmail.com>
2022-08-29 16:05:53 -0300

    exercicio

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PedraPapelAtaque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rodadas =int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < rodadas; i++)
+            {
+                string jogadorUm = (Console.ReadLine());
+                string jogadorDois = (Console.ReadLine());
+
+                if (jogadorUm.Equals("ataque") && jogadorDois.Equals("ataque")) Console.WriteLine("Aniquilacao mutua");
+                else if (jogadorUm.Equals("papel") && jogadorDois.Equals("papel")) Console.WriteLine("Ambos venceram");
+                else if (jogadorUm.Equals("ataque"))Console.WriteLine("Jogador 1 venceu");
+                else if (jogadorDois.Equals("ataque")) Console.WriteLine("Jogador 2 venceu");
+                else if (jogadorDois.Equals("papel"))Console.WriteLine("Jogador 1 venceu");
+                else if (jogadorUm.Equals("papel")) Console.WriteLine("Jogador 2 venceu");
+                else Console.WriteLine("Sem ganhador");
+
+            }
+        }
+    }
+}
